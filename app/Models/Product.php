ba5b0a222b39de650ba3b582cb67d4ba311f78db
FishingDonut <106450510+FishingDonut@users.noreply.github.com>
2025-01-04 12:17:39 -0300

    create model product

---
+++ app/Models/Product.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $table = 'product';
+
+    protected $fillable = [
+        'id',
+        'fullName',
+        'price',
+        'price_multiplier',
+        'description'
+    ];
+}
