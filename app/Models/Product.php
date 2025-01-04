154a7f45ece075ef81e87cac1dfab386e0ebe57f
FishingDonut <106450510+FishingDonut@users.noreply.github.com>
2025-01-04 12:20:58 -0300

    update model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -15,7 +15,7 @@ class Product extends Model
 
     protected $fillable = [
         'id',
-        'fullName',
+        'name',
         'price',
         'price_multiplier',
         'description'
