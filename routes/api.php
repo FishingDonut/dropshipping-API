633a97b9297640f3ae283b540fc5e2649f52b3d7
FishingDonut <106450510+FishingDonut@users.noreply.github.com>
2025-01-06 12:07:46 -0300

    create router product

--- routes/api.php
+++ routes/api.php
@@ -3,3 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::resource('/user', App\Http\Controllers\UserController::class);
+Route::resource('/product', App\Http\Controllers\ProductController::class);
