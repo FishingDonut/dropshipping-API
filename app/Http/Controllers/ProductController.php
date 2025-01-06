52adea682ffa2a6dcd966a57af2fc99499b41951
FishingDonut <106450510+FishingDonut@users.noreply.github.com>
2025-01-06 12:07:25 -0300

    create controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,26 +2,33 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Product;
-use App\Http\Requests\StoreProductRequest;
+use App\Services\ProductService as Service;
 use App\Http\Requests\UpdateProductRequest;
+use App\Http\Requests\StoreProductRequest;
+use Illuminate\Http\Response;
+use Exception;
 
 class ProductController extends Controller
 {
+    protected $service;
+
+    public function __construct(Service $service)
+    {
+        $this->service = $service;
+    }
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
-    }
+        try {
+            $response = $this->service->getAll();
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+            return response()->json($response, Response::HTTP_OK);
+        } catch (Exception $th) {
+            return response()->json($th->getMessage());
+        }
     }
 
     /**
@@ -29,38 +36,67 @@ class ProductController extends Controller
      */
     public function store(StoreProductRequest $request)
     {
-        //
+        try {
+            $response = $this->service->store($request);
+
+            return response()->json($response, Response::HTTP_CREATED);
+        } catch (Exception $th) {
+            return response()->json($th->getMessage());
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Product $product)
+    public function show($id)
     {
-        //
-    }
+        try {
+            $response = $this->service->findOne($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Product $product)
-    {
-        //
+            if (!$response) {
+                return response()->json("Not Found.", Response::HTTP_NOT_FOUND);
+            }
+
+            return response()->json($response, Response::HTTP_OK);
+        } catch (Exception $th) {
+            return response()->json($th->getMessage());
+        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateProductRequest $request, Product $product)
+
+    public function update(UpdateProductRequest $request, $id)
     {
-        //
+        try {
+            $response = $this->service->update($request->all(), $id);
+
+            if (!$response) {
+                return response()->json("Not Found.", Response::HTTP_NOT_FOUND);
+            }
+
+            return response()->json($response, Response::HTTP_NO_CONTENT);
+        } catch (Exception $th) {
+            return response()->json($th->getMessage());
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Product $product)
+    public function destroy($id)
     {
-        //
+        try {
+            $response = $this->service->delete($id);
+
+            if (!$response) {
+                return response()->json("Not Found.", Response::HTTP_NOT_FOUND);
+            }
+
+            return response()->json($response, Response::HTTP_NO_CONTENT);
+        } catch (Exception $th) {
+            return response()->json($th->getMessage());
+        }
     }
 }
