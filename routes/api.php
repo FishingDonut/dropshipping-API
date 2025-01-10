33543d33008ade00c0ea52d969c55ce2d88b6ba7
FishingDonut <protronsdimi@gmail.com>
2025-01-10 18:45:38 -0300

    update

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Models\{
-    Category,
-    Field,
-    FieldValues
-};
-
 Route::resource('/user', App\Http\Controllers\UserController::class);
 Route::resource('/product', App\Http\Controllers\ProductController::class);
