7eb0a12f7d2ea002094c4fc58cd7c57da8b81fc9
FishingDonut <protronsdimi@gmail.com>
2025-01-10 20:21:27 -0300

    update form

---
+++ app/Http/Requests/UpdateCategoryRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+class UpdateCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'message' => 'failedValidation',
+            'errors' => $validator->errors(),
+        ], 422));
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|min:4|unique:categories,name'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'The name field is required.',
+            'name.min' => 'The name must be at least 4 characters.'
+        ];
+    }
+}
