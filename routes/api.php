458ab85f234e2d2508da87e6eab37a3a975c020a
FishingDonut <protronsdimi@gmail.com>
2025-01-10 18:45:23 -0300

    clean api

--- routes/api.php
+++ routes/api.php
@@ -10,9 +10,3 @@ use App\Models\{
 
 Route::resource('/user', App\Http\Controllers\UserController::class);
 Route::resource('/product', App\Http\Controllers\ProductController::class);
-
-
-Route::get('category', function (){
-    $values = Category::with('fields.field_values.product')->find(1);
-    return $values;
-});
