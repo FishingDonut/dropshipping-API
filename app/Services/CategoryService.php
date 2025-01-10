05957eeda9ae6d4594f13db8a6e757e47fe9d920
FishingDonut <protronsdimi@gmail.com>
2025-01-10 20:19:14 -0300

    create service category

---
+++ app/Services/CategoryService.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Category as Model;
+use Exception;
+
+class CategoryService
+{
+    protected $model;
+
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    function getAll()
+    {
+        try {
+            return $this->model->all();
+        } catch (Exception $th) {
+            return $th->getMessage();
+        }
+    }
+    function findOne($id)
+    {
+        try {
+            return $this->model->where('id', $id)->first();
+        } catch (Exception $th) {
+            return $th->getMessage();
+        }
+    }
+
+    function update($request, $id)
+    {
+        try {
+            $response = $this->model->where('id', $id)->first();
+
+            if (!$response) {
+                return null;
+            }
+
+            return $response->update($request);
+        } catch (Exception $th) {
+            return $th->getMessage();
+        }
+    }
+
+    function store($request)
+    {
+        try {
+            return $this->model->create($request->all());
+        } catch (Exception $th) {
+            return $th->getMessage();
+        }
+    }
+    function delete($id)
+    {
+        try {
+            $response = $this->model->where('id', $id)->first();
+            if (!$response) {
+                return null;
+            }
+            return $response->delete();
+        } catch (Exception $th) {
+            return $th->getMessage();
+        }
+    }
+}
