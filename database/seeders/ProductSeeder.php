7789390e57fd9ca15bc58d11228afa2c506fb365
FishingDonut <protronsdimi@gmail.com>
2025-01-11 13:09:19 -0300

    update seeder product

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -13,6 +13,9 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        Product::factory()->count(5)->create();
+        Product::factory()->state(["name" => "S23", "price" => 4999.90, "price_multiplier" => 1.2, "description" => "Melhor celular"])->create();
+        Product::factory()->state(["name" => "S24", "price" => 7599.50, "price_multiplier" => 1.7, "description" => "Celular de 2024"])->create();
+        Product::factory()->state(["name" => "Camisa 1", "price" => 39.90, "price_multiplier" => 2.5, "description" => "Camisa basica"])->create();
+        Product::factory()->count(2)->create();
     }
 }
